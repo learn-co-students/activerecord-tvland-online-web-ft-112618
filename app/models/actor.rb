934c66fa2447228460b50c95f2e331aba3decdc2
Jon <jhharwood88@gmail.com>
2019-01-28 17:34:10 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+	has_many :characters
+	has_many :shows, through: :characters
+
+	def full_name
+		full_name = "#{self.first_name} #{self.last_name}"
+		full_name
+	end
+
+	def list_roles
+		names = []
+    	self.shows.each do |x|
+    	names << x.name
+    	end
+    	names
+	end
+  	
 end
---
+++ db/migrate/004_add_actor_name.rb
@@ -0,0 +1,9 @@
+#Write a migration for the actors table. An actor should have a first_name and a last_name.
+class AddActorName < ActiveRecord::Migration[4.2]
+	def change
+		create_table :actors do |t|
+			t.string :first_name
+			t.string :last_name
+		end
+	end
+end
